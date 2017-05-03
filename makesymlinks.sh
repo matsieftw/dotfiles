f7990670819dbeb13888a5c7b8cefb8a49918149
Mattie Kenny <Meagan.Kenny@nike.com>
2017-05-03 15:00:08 -0700

    Fix Merge Conflict.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,7 +11,6 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
 files="vimrc tmux.conf zshenv" # list of files/folders to symlink in homedir
-
 ##########
 
 # create dotfiles_old in homedir
@@ -28,6 +27,29 @@ echo "done"
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
+		mv ~/.zsh ~/dotfiles_old/zsh/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# create zsh directory and zsh functions directory
+echo -n "Creating zsh directory."
+mkdir -p ~/.zsh/
+echo "done"
+
+# change into the zsh dotfile directory
+echo -n "Changing into the zsh directory."
+cd ~/dotfiles/zsh/
+echo "done"
+
+# symlink zsh files
+for file in *; do
+	echo "Creating symlink to $file in zsh directory."
+	ln -s $dir/zsh/$file ~/.zsh/.$file
+	echo "done"
+done
+
+# create symlink to functions directory
+echo "Creating symlink to functions directory."
+ln -s $dir/zsh/functions ~/.zsh/
+echo "done"
